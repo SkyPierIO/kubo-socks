7a433cb57dc0fb267b4cfc0c1e90dd2fcf28e653
smolgroot <nautilushell@protonmail.com>
2023-10-26 23:43:38 +0200

    updating build script for v0.1.2

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 # WINDOWS
 # 64-bit
-GOOS=windows GOARCH=amd64 go build -o build/kubo-socks-0.1.1-windows-amd64.exe -ldflags "-s -w"
+GOOS=windows GOARCH=amd64 go build -o build/kubo-socks-0.1.2-windows-amd64.exe -ldflags "-s -w"
 # 32-bit
-GOOS=windows GOARCH=386 go build -o build/kubo-socks-0.1.1-windows-i386.exe -ldflags "-s -w"
+GOOS=windows GOARCH=386 go build -o build/kubo-socks-0.1.2-windows-i386.exe -ldflags "-s -w"
 
 # LINUX
 # 64-bit
-GOOS=linux GOARCH=amd64 go build -o build/kubo-socks-0.1.1-linux-amd64 -ldflags "-s -w"
+GOOS=linux GOARCH=amd64 go build -o build/kubo-socks-0.1.2-linux-amd64 -ldflags "-s -w"
 # 32-bit
-GOOS=linux GOARCH=386 go build -o build/kubo-socks-0.1.1-linux-i386 -ldflags "-s -w"
+GOOS=linux GOARCH=386 go build -o build/kubo-socks-0.1.2-linux-i386 -ldflags "-s -w"
 
 # DARWIN - MACOS
 # 64-bit
-GOOS=darwin GOARCH=amd64 go build -o build/kubo-socks-0.1.1-darwin-amd64 -ldflags "-s -w"
+GOOS=darwin GOARCH=amd64 go build -o build/kubo-socks-0.1.2-darwin-amd64 -ldflags "-s -w"
 # 32-bit
-# GOOS=darwin GOARCH=386 go build -o build/kubo-socks-0.1.1-darwin-i386 -ldflags "-s -w"
+# GOOS=darwin GOARCH=386 go build -o build/kubo-socks-0.1.2-darwin-i386 -ldflags "-s -w"
 
